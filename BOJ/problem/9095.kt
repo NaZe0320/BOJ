ebbe122be74ec099403ee292e3f7b56ba1917925
NaZe0320 <rwt1226@naver.com>
2023-02-22 16:49:13 +0900

    2023.02.22 푼 문제

---
+++ BOJ/problem/9095.kt
@@ -0,0 +1,22 @@
+package problem
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val dp = Array(11) {0}
+    dp[1] = 1; dp[2] = 2; dp[3] = 4
+
+    repeat(br.readLine().toInt()) {
+        val n = br.readLine().toInt()
+
+        for (i in 4..n) {
+            if (dp[i] == 0) {
+                dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+            }
+        }
+        bw.write("${dp[n]}\n")
+    }
+    bw.flush()
+    bw.close()
+}
