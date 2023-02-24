5ebe6fab161870c446d3dba53c93f9cadd462669
NaZe0320 <rwt1226@naver.com>
2023-02-25 08:41:25 +0900

    2023.02.25 푼 문제

---
+++ BOJ/problem/9461.kt
@@ -0,0 +1,26 @@
+package problem
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val t = br.readLine().toInt()
+
+    repeat(t) {
+        val n = br.readLine().toInt()
+
+        val dp = Array(n+1) {0L}
+
+        dp[1] = 1;
+        if (n>1) dp[2] = 1
+        if (n>2) dp[3] = 1
+        if (n>3) dp[4] = 2
+
+        for (i in 5..n) {
+            dp[i] = (dp[i-5] + dp[i-1])
+        }
+        bw.write("${dp[n]}\n")
+    }
+    bw.flush()
+    bw.close()
+}
