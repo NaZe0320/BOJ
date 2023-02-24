ab976bdfb31cd138324881ef3743f9df47103dd2
NaZe0320 <rwt1226@naver.com>
2023-02-24 15:53:50 +0900

    2023.02.24 푼 문제

---
+++ BOJ/problem/11727.kt
@@ -0,0 +1,20 @@
+package problem
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val n = br.readLine().toInt()
+
+    val dp = Array(n+1) {0}
+
+    dp[1] = 1;
+    if (n>1) dp[2] = 3
+
+    for (i in 3..n) {
+        dp[i] = (dp[i-1] + 2*dp[i-2])%10007
+    }
+    bw.write("${dp[n]}")
+    bw.flush()
+    bw.close()
+}
